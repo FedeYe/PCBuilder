124a1e4068c0559a3ce904c791a5de1c45a7be16
Mattia Pandolfo <mattia.pandolfo@studenti.unipd.com>
2023-03-30 16:23:04 +0200

    aggiunta Logger

---
+++ src/Service/Logger/Level.h
@@ -0,0 +1,20 @@
+#ifndef SERVICE_LOGGER_LEVEL_H
+#define SERVICE_LOGGER_LEVEL_H
+
+namespace Service
+{
+    namespace Logger
+    {
+
+        enum class Level
+        {
+            Debug,
+            Info,
+            Warning,
+            Error
+        };
+
+    }
+}
+
+#endif
